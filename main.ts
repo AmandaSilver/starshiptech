233d591c615a8799ef5264784465e0522f00dbf7
Amanda Silver <amanda.silver@microsoft.com>
2021-12-18 11:51:09 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+let asteroid: Sprite = null
+effects.starField.startScreenEffect()
+let mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 7 . . . . . . . 
+    . . . . . . 8 8 5 6 . . . . . . 
+    . . . . . . 8 7 5 6 . . . . . . 
+    . . . . . c c c 6 6 6 . . . . . 
+    . . . . 8 8 7 7 7 5 6 6 . . . . 
+    . . 8 f f f c c 6 6 f f 6 6 . . 
+    . 8 8 8 8 6 6 7 7 7 7 5 7 6 6 . 
+    8 8 8 8 8 8 6 6 7 7 7 5 7 7 6 6 
+    8 8 8 8 8 8 6 6 7 7 7 7 5 7 6 6 
+    `, SpriteKind.Player)
+mySprite.setPosition(80, 111)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    asteroid = sprites.createProjectileFromSide(img`
+        . . . . . . . c c c a c . . . . 
+        . . c c b b b a c a a a c . . . 
+        . c c a b a c b a a a b c c . . 
+        . c a b c f f f b a b b b a . . 
+        . c a c f f f 8 a b b b b b a . 
+        . c a 8 f f 8 c a b b b b b a . 
+        c c c a c c c c a b c f a b c c 
+        c c a a a c c c a c f f c b b a 
+        c c a b 6 a c c a f f c c b b a 
+        c a b c 8 6 c c a a a b b c b c 
+        c a c f f a c c a f a c c c b . 
+        c a 8 f c c b a f f c b c c c . 
+        . c b c c c c b f c a b b a c . 
+        . . a b b b b b b b b b b b c . 
+        . . . c c c c b b b b b c c . . 
+        . . . . . . . . c b b c . . . . 
+        `, 0, 50)
+    asteroid.x = randint(0, scene.screenWidth())
+})
